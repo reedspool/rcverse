ba9bd25135be40ab8b39ca49214c41117ac760d5
Human <reedwith2es@gmail.com>
2024-05-14 15:43:22 -0700

    Cache user images

--- public/service-worker-driver.js
+++ public/service-worker-driver.js
@@ -12,12 +12,31 @@ function extractCookieByName(cookie, name) {
 }
 
 const RCVERSE_SERVICE_WORKER_CACHE_NAME = "rcverse-service-worker-cache-v4";
+// Adapted from an MDN example
 const deleteOldCaches = async () => {
   const cacheKeepList = [RCVERSE_SERVICE_WORKER_CACHE_NAME];
   const keyList = await caches.keys();
   const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
   await Promise.all(cachesToDelete.map((key) => caches.delete(key)));
 };
+
+// Adapted from
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers#recovering_failed_requests
+const putInCache = async (request, response) => {
+  const cache = await caches.open(RCVERSE_SERVICE_WORKER_CACHE_NAME);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+};
+
 self.addEventListener("install", async function (event) {
   console.log(
     `Service worker installed. Initiating cache ${RCVERSE_SERVICE_WORKER_CACHE_NAME}`,
@@ -64,13 +83,16 @@ self.addEventListener("fetch", async function (event) {
       decodeURIComponent(personalizationsCookie),
     );
 
-    console.log("Caching all personzliations", personalizations);
-
     event.waitUntil(
       caches.open(RCVERSE_SERVICE_WORKER_CACHE_NAME).then(function (cache) {
         // TODO: Are we forcing re-request of all these things? Don't refresh
         //       any which were already in cache
         // TODO: How would these things be cleaned up?
+        // TODO: In my vision for duplicating the back-end routes into this
+        //       service worker, the Personalizations cookie modifications might
+        //       be captured and entirely performed on the front-end. Unfortunately
+        //       that seems to be explicitly rejected as a usecase currently.
+        //       See https://stackoverflow.com/a/44445217
         return cache.addAll(personalizations);
       }),
     );
@@ -79,6 +101,19 @@ self.addEventListener("fetch", async function (event) {
 
   // Let the browser do its default thing for non-GET requests not matched above.
   if (event.request.method !== "GET") return;
+
+  // Some things we want to cache after the first time we get them
+  if (
+    // If it's a user photo/asset served or proxied through RC's infra
+    event.request.url.match(/https:\/\/[^.].cloudfront.net\/assets\//) ||
+    // There's two URLs by which these are served?
+    event.request.url.match(
+      /https:\/\/assets.recurse.com\/rails\/active_storage\/representations/,
+    )
+  ) {
+    event.respondWith(cacheFirst(event.request));
+    return;
+  }
 
   // Default, check the cache or just go with the original
   event.respondWith(
